d74ceb9c4685b274a7dfe699f8f98490fdb88f9c
Nikolay Kostov <admin@nikolay.it>
2015-10-19 18:47:09 +0300

    Created new default ASP.NET MVC application OnlineGames.Web.AiPortal.csproj

---
+++ Source/Web/OnlineGames.Web.AiPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineGames.Web.AiPortal.Startup))]
+namespace OnlineGames.Web.AiPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
