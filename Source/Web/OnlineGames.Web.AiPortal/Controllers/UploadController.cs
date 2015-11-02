4d550384d310c7d43b4b8101a54b21864bba2fb6
Nikolay Kostov <admin@nikolay.it>
2015-11-02 19:49:00 +0200

    Created controller and view for Upload

---
+++ Source/Web/OnlineGames.Web.AiPortal/Controllers/UploadController.cs
@@ -0,0 +1,28 @@
+﻿namespace OnlineGames.Web.AiPortal.Controllers
+{
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using OnlineGames.Data.Common;
+    using OnlineGames.Data.Models;
+    using AutoMapper.QueryableExtensions;
+
+    using OnlineGames.Web.AiPortal.ViewModels.Teams;
+
+    [Authorize]
+    public class UploadController : BaseController
+    {
+        private readonly IDbRepository<Team> teamsRepository;
+
+        public UploadController(IDbRepository<Team> teamsRepository)
+        {
+            this.teamsRepository = teamsRepository;
+        }
+
+        public ActionResult Index(int id)
+        {
+            var team = this.teamsRepository.All().Where(x => x.Id == id).ProjectTo<TeamInfoViewModel>().FirstOrDefault();
+            return this.View(team);
+        }
+    }
+}
