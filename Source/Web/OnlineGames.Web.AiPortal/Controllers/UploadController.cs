4985f018a7a9b4b6869db9d704ee29e008d91731
Nikolay Kostov <admin@nikolay.it>
2015-11-02 20:14:47 +0200

    [Work in progress] Uploading files

--- Source/Web/OnlineGames.Web.AiPortal/Controllers/UploadController.cs
+++ Source/Web/OnlineGames.Web.AiPortal/Controllers/UploadController.cs
@@ -1,12 +1,15 @@
 ﻿namespace OnlineGames.Web.AiPortal.Controllers
 {
+    using System.IO;
     using System.Linq;
+    using System.Net;
+    using System.Web;
     using System.Web.Mvc;
+
+    using AutoMapper.QueryableExtensions;
 
     using OnlineGames.Data.Common;
     using OnlineGames.Data.Models;
-    using AutoMapper.QueryableExtensions;
-
     using OnlineGames.Web.AiPortal.ViewModels.Teams;
 
     [Authorize]
@@ -19,10 +22,37 @@
             this.teamsRepository = teamsRepository;
         }
 
+        [HttpGet]
         public ActionResult Index(int id)
         {
-            var team = this.teamsRepository.All().Where(x => x.Id == id).ProjectTo<TeamInfoViewModel>().FirstOrDefault();
+            // TODO: Replace this.User.Identity.Name with identity provider for easier unit testing
+            var team =
+                this.teamsRepository.All()
+                    .Where(x => x.Id == id && x.TeamMembers.Any(tm => tm.User.UserName == this.User.Identity.Name))
+                    .ProjectTo<TeamInfoViewModel>()
+                    .FirstOrDefault();
+
+            if (team == null)
+            {
+                return new HttpStatusCodeResult(
+                    HttpStatusCode.Forbidden,
+                    "You do not have permissions to upload files for this team!");
+            }
+
             return this.View(team);
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            if (file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                file.SaveAs(path);
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Source/Web/OnlineGames.Web.AiPortal/ViewModels/Upload/FileUploadInputModel.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineGames.Web.AiPortal.ViewModels.Upload
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class FileUploadInputModel
+    {
+        // TODO: Add URL validation
+        [Required]
+        public string SourceCodeRepository { get; set; }
+
+        public FileUploadInputModel File { get; set; }
+    }
+}
