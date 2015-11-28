1162547b8615c5efcc422561c9e6820b0c3c7019
Nikolay Kostov <admin@nikolay.it>
2015-11-28 21:20:42 +0200

    Added migration for battle changes

---
+++ Source/Data/OnlineGames.Data/Migrations/201511281917526_Battles.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="201511281917526_Battles.cs" company="Nikolay Kostov (Nikolay.IT)">
+// Copyright (c) Nikolay Kostov (Nikolay.IT). All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// </copyright>
+
+namespace OnlineGames.Data.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class Battles : DbMigration
+    {
+        public override void Up()
+        {
+            this.AddColumn("dbo.Competitions", "GamesToPlayForEachBattle", c => c.Int(nullable: false));
+        }
+
+        public override void Down()
+        {
+            this.DropColumn("dbo.Competitions", "GamesToPlayForEachBattle");
+        }
+    }
+}
