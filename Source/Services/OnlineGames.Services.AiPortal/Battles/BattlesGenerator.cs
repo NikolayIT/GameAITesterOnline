b43668b78eb015230610718064a5cfac12d40f7b
Nikolay Kostov <admin@nikolay.it>
2015-11-28 20:45:45 +0200

    Creating battles implemented in BattlesGenerator

---
+++ Source/Services/OnlineGames.Services.AiPortal/Battles/BattlesGenerator.cs
@@ -0,0 +1,35 @@
+﻿namespace OnlineGames.Services.AiPortal.Battles
+{
+    using System.Linq;
+
+    using OnlineGames.Data.Common;
+    using OnlineGames.Data.Models;
+
+    public class BattlesGenerator : IBattlesGenerator
+    {
+        public void GenerateBattles(IDbRepository<Team> teamsRepository, IDbRepository<Battle> battlesRepository, int competitionId)
+        {
+            var teams = teamsRepository.All().Where(x => x.CompetitionId == competitionId).Select(x => new { x.Id }).ToList();
+            for (var i = 0; i < teams.Count; i++)
+            {
+                for (int j = i + 1; j < teams.Count; j++)
+                {
+                    var firstTeam = teams[i];
+                    var secondTeam = teams[j];
+                    if (battlesRepository.All().Any(
+                        x =>
+                        (x.FirstTeamId == firstTeam.Id && x.SecondTeamId == secondTeam.Id)
+                        || (x.FirstTeamId == secondTeam.Id && x.FirstTeamId == firstTeam.Id)))
+                    {
+                        // Battle already exists
+                        continue;
+                    }
+
+                    battlesRepository.Add(new Battle { FirstTeamId = firstTeam.Id, SecondTeamId = secondTeam.Id });
+                }
+            }
+
+            battlesRepository.Save();
+        }
+    }
+}
---
+++ Source/Services/OnlineGames.Services.AiPortal/Battles/IBattlesGenerator.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineGames.Services.AiPortal.Battles
+{
+    using OnlineGames.Data.Common;
+    using OnlineGames.Data.Models;
+
+    public interface IBattlesGenerator
+    {
+        void GenerateBattles(IDbRepository<Team> teamsRepository, IDbRepository<Battle> battlesRepository, int competitionId);
+    }
+}
